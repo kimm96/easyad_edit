ca80a63eb99cb0fc22c1937119824023f67ef91a
Kimm <kmdeen23@gmail.com>
2017-07-20 15:43:13 +0000

    모델추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
 class HomeController < ApplicationController
   def edit1
   end
-
+  
+  def write
+    lion = Post.new
+    lion.title = params["title"]
+    lion.detail = params["detail"]
+    lion.address = params["address"]
+    lion.save
+    redirect_to '/edit2/' + lion.id.to_s #따로 목록없음 걍 바로 보여주고 끝 (아이디값 이용)
+  end
+  
   def edit2
+     @post = Post.find(params["id"]) #아이디값 받아오기
   end
 end
